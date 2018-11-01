aba2fe4374c61286ee14670028e08effd3eba884
Ethan <ethan.r.davidson@gmail.com>
2018-11-01 00:39:07 -0700

    Lab thing worked!!

---
+++ neopets/trudy/index.js
@@ -0,0 +1,24 @@
+// http://www.neopets.com/trudydaily/slotgame.phtml
+
+// fetch('https://api.github.com/gists', {
+//     method: 'post',
+//     body: JSON.stringify(opts)
+//   }).then(function(response) {
+//     return response.json();
+//   }).then(function(data) {
+//     ChromeSamples.log('Created Gist:', data.html_url);
+//   });
+// var ajaxurl = '/trudydaily/ajax/claimprize.php';
+//       //var data = {'action': 'getslotstate'};
+//       var data = {
+//         'action': 'getslotstate','key': '0'};
+//       var resp;
+//       parent.$.post(ajaxurl, data, function (response){
+//         resp = JSON.parse(response);
+//                     SlotsGame.LoadAssets(AssetLoadComplete, "http://images.neopets.com");
+//                 }
+//         );
+//         SlotsGame.Initialize();
+//         function AssetLoadComplete(){
+//           SlotsGame.Start(resp);
+//         }
