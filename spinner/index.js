c0fc1e03f4fd39894418d2284c7a9cc26011fb2b
Ethan <ethan.r.davidson@gmail.com>
2018-11-07 18:05:51 -0800

    added bueno comments

--- spinner/index.js
+++ spinner/index.js
@@ -3,3 +3,5 @@ const puppeteer = require('puppeteer');
 
 // https://articlerewritertool.com/
 // But I have to solve a math question...
+//  Try https://github.com/finnfiddle/words-to-numbers
+//  and https://github.com/josdejong/mathjs
