573087ac3cc7941dab641d31fdd26bda0c163903
Ethan <ethan.r.davidson@gmail.com>
2018-11-07 18:34:21 -0800

    yuh yuh yuh strats strats

--- spinner/index.js
+++ spinner/index.js
@@ -5,3 +5,18 @@ const puppeteer = require('puppeteer');
 // But I have to solve a math question...
 //  Try https://github.com/finnfiddle/words-to-numbers
 //  and https://github.com/josdejong/mathjs
+
+const tests = ["five - five =", "eight minus five =", "five minus ten =", "seven plus 3 =", "7 plus 5 =", "one minus one ="];
+
+// helper functions
+
+const solve_math_question(text) {
+  let tokens = text.split(" ");
+  tokens.map(token => {
+    token.replace(/minus/g, "-");
+    token.replace(/plus/g, "+");
+    token = token; // convert to num
+  });
+  return 0; // math.solve(tokens.join(" "))
+
+}
