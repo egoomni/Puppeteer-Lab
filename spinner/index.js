c4629726d778b3f92fb4e8d68c9faa8db68caf9b
Ethan <ethan.r.davidson@gmail.com>
2018-11-08 20:17:33 -0800

    ADDED working solve_math_question helper function

--- spinner/index.js
+++ spinner/index.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
+const math = require('mathjs');
+const {wordsToNumbers} = require('words-to-numbers');
 const puppeteer = require('puppeteer');
 
 // https://articlerewritertool.com/
-// But I have to solve a math question...
-//  Try https://github.com/finnfiddle/words-to-numbers
-//  and https://github.com/josdejong/mathjs
 
-const tests = ["five - five =", "eight minus five =", "five minus ten =", "seven plus 3 =", "7 plus 5 =", "one minus one ="];
-
-// helper functions
-
-const solve_math_question(text) {
-  let tokens = text.split(" ");
-  tokens.map(token => {
-    token.replace(/minus/g, "-");
-    token.replace(/plus/g, "+");
-    token = token; // convert to num
+// HELPERS
+const solve_math_question = (text) => {
+  const tokens = text.split(" ").map(token => {
+    if (token == "minus") return "-";
+    else if (token == "plus") return "+";
+    else return wordsToNumbers(token);
   });
-  return 0; // math.solve(tokens.join(" "))
-
-}
+  const expression = tokens.slice(0, tokens.length - 1).join(" ");
+  return math.parse(expression).eval();
+};
