f81c8dc571c555f3debd166cacac05c2cdff0896
Ethan <ethan.r.davidson@gmail.com>
2018-11-09 05:21:58 -0800

    rpwerfsdkjfsOCKhfSDIJXK

--- spinner/index.js
+++ spinner/index.js
@@ -1,17 +1,50 @@
 const fs = require('fs');
+const rl = require('readline-sync');
 const math = require('mathjs');
 const {wordsToNumbers} = require('words-to-numbers');
 const puppeteer = require('puppeteer');
 
-// https://articlerewritertool.com/
-
-// HELPERS
 const solve_math_question = (text) => {
   const tokens = text.split(" ").map(token => {
     if (token == "minus") return "-";
     else if (token == "plus") return "+";
     else return wordsToNumbers(token);
   });
-  const expression = tokens.slice(0, tokens.length - 1).join(" ");
+  const expression = tokens.join(" ");
   return math.parse(expression).eval();
 };
+
+const whereto = "https://articlerewritertool.com/";
+const paragraphs = fs.readFileSync(rl.question("Where is your text file?\n> "), "utf8").split("\n");
+let result = "";
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  console.log(`Page going to ${whereto}`);
+  await page.goto(whereto);
+
+  for (let paragraph of paragraphs) {
+
+    console.log(paragraph);
+
+    await page.focus("textarea[name='formNameLabelTextBefore']");
+    await page.keyboard.type(paragraph);
+
+    let ans = await page.$eval("#math_captcha_equation", input => input.value);
+    ans = solve_math_question(ans);
+    await page.focus("textarea[name='formNameLabelTextBefore']");
+    await page.keyboard.type(answer);
+
+    await page.click("input[value='Rewrite Text']");
+    result += await page.$eval("textarea[name='formNameLabelTextAfter']", textarea => textarea.value);
+    result += "\n";
+
+  }
+
+  fs.writeFileSync("spinned.txt", result);
+  console.log("saved");
+
+})();
